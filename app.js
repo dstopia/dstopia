d6c55a945b5a13dfa7c193b3f75e9aafcab370ae
dfsdanimaulana <danimaulana9f@gmail.com>
2021-09-17 19:44:40 +0700

    setting express server

---
+++ app.js
@@ -0,0 +1,38 @@
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./src/routes/index'))
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.render('error')
+})
+
+module.exports = app
---
+++ src/routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+/* GET signup page. */
+router.get('/signup', function (req, res) {
+    res.render('signup')
+})
+
+/* GET login page. */
+router.get('/login', function (req, res) {
+    res.render('login')
+})
+
+/* GET User page. */
+router.get('/:username', function (req, res) {
+    // mengambil username dari url
+    const username = req.params.username
+    res.render('user', { username })
+})
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index')
+})
+
+module.exports = router
