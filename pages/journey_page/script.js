2ba899e368ed9ef20c1cd6c206e84a2f741c3045
Irullatif <irulsoekamti94@gmail.com>
2021-09-20 16:39:04 +0700

    Update Journey Page
    ganti card model

---
+++ pages/journey_page/script.js
@@ -0,0 +1,11 @@
+$(window).load(function () {
+  $(".post-module").hover(function () {
+    $(this).find(".description").stop().animate(
+      {
+        height: "toggle",
+        opacity: "toggle",
+      },
+      300
+    );
+  });
+});
